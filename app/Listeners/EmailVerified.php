596fa4d7de7131cc54c0356ecdf34d81ed0cb52e
洪学臣 <stevehum14@gmail.com>
2020-02-19 13:22:23 +0800

    fix

--- app/Listeners/EmailVerified.php
+++ app/Listeners/EmailVerified.php
@@ -27,6 +27,6 @@ class EmailVerified
     public function handle(Verified $event)
     {
         // 会话里闪存认证成功后的消息提醒
-        session()->flash('success', '邮箱验证成功 ^_^');
+        session()->flash('success', '邮箱验证成功 ^_^11');
     }
 }
